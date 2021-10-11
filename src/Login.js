86607c137c91e3af0c21368a03370f1ffafd3eab
nagarajugoud151998 <nagarajugoud151998@gmail.com>
2021-10-11 12:53:29 +0530

    six commit

--- src/Login.js
+++ src/Login.js
@@ -5,9 +5,9 @@ function Login() {
     return (
         <div>
         Enter your Email<input type="text" placeholder="Enter your name" style={{margin:'32px'}}></input><br/>
-        Enter your Password <input type="password" placeholder="password" ></input><br/>
-        <button type="search" style={{margin:'50px',background:'blue'}}><Link to='class1'>Submit</Link></button>
-       <h1 className="class1">successfully complted</h1>
+        Enter yourPassword <input type="password" placeholder="password" ></input><br/>
+        <button type="search" style={{margin:'45px',background:'blue'}}><Link to='class1'>Submit</Link></button>
+       <h1 className="class1">successfull complted</h1>
         </div>
     )
 }
